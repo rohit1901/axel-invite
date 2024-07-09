ec3ca7fec78a9329f4f1f7c1605cfef28cca8bca
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-07-09 16:25:33 +0200

    feat: modal

---
+++ src/Modal.tsx
@@ -0,0 +1,20 @@
+import {HTMLAttributes, PropsWithChildren} from "react";
+
+type ModalProps = {
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+    header?: string;
+} & HTMLAttributes<HTMLDialogElement>;
+export const Modal = ({isOpen, setIsOpen, header = "Hello!", children}: PropsWithChildren<ModalProps>) => {
+    return (
+        <dialog id="my_modal_2" className="modal" open={isOpen}>
+            <div className="modal-box">
+                <h3 className="font-bold text-lg">{header}</h3>
+                <p className="py-4">{children}</p>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button onClick={() => setIsOpen(!isOpen)}>Close</button>
+            </form>
+        </dialog>
+    )
+}
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,11 +1,19 @@
 import Avatar from "./assets/avatar.jpeg"
+import MapIcon from "./assets/map.svg"
+import {useState} from "react";
+import {Modal} from "./Modal.tsx";
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const header = "Die Party findet am 12.07.2024 um 14 Uhr statt.";
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">AXEL MÜLLER</a>
             </div>
             <div className="flex-none gap-2">
+                <div className="btn btn-ghost" role="button" onClick={() => setIsOpen(!isOpen)}>
+                    <img alt="Map Icon" src={MapIcon} className="w-6 h-6"/>
+                </div>
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                     <div className="w-10 rounded-full">
                         <img
@@ -14,6 +22,20 @@ export const Navbar = () => {
                     </div>
                 </div>
             </div>
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} header={header}>
+                <p>Die Party findet in Radebeul bei Thomas statt. Die genaue Adresse lautet: Hauptstraße 1, 12345 Musterstadt.</p>
+                <div className="mapouter">
+                    <div className="gmap_canvas p-5">
+                        <iframe
+                            title="Google Maps"
+                            width="100%"
+                            height="300"
+                            id="gmap_canvas"
+                            src="https://maps.google.com/maps?q=Hauptstra%C3%9Fe%201%2C%2012345%20Musterstadt&t=&z=13&ie=UTF8&iwloc=&output=embed">
+                        </iframe>
+                    </div>
+                </div>
+            </Modal>
         </div>
     );
 }
